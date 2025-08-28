33d9148c4018d70dea151b9ba6cacffca3e0f5c1
SCMads <64943602+SCMads@users.noreply.github.com>
2025-08-28 12:51:42 -0300

    Create App.jsx with basic React component structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Network Mapper</h1>
+            <p>Welcome to the Network Mapper application!</p>
+        </div>
+    );
+};
+
+export default App;
